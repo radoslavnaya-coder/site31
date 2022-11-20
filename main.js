4f6800b1fa0f8984d9af7067b7afe45d1479b32f
radoslavnaya-coder <killaarte13@gmail.com>
2022-11-20 21:59:40 +0400

    20.11 upd

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let id = null;
+
+
 async function getPosts() {
     let res = await fetch('http://api.site31.ru/posts');
     let posts = await res.json();
@@ -8,7 +11,7 @@ async function getPosts() {
             <div class="card-body">
                 <h5 class="card-title">${post.title}</h5>
                 <p class="card-text">${post.body}</p>
-                <a href="#" class="card-link">Подробнее</a>
+                <a href="#" class="card-link" onclick="selectPost('${post.id}', '${post.title}','${post.body}')">Редактировать пост</a>
                 <a href="#" class="card-link" onclick="removePost(${post.id})">Удалить</a>
             </div>
         </div>
@@ -18,7 +21,7 @@ async function getPosts() {
 
 async function addPost() {
     const title = document.getElementById('title').value,
-    body = document.getElementById('body').value;
+      body = document.getElementById('body').value;
 
     let formData = new FormData();
     formData.append('title', title);
@@ -36,10 +39,35 @@ async function addPost() {
 
 async function removePost(id) {
     const res = await fetch(`http://api.site31.ru/posts/${id}`, {
-        method: 'DELETE'
+         method: 'DELETE'
     });
     const data = await res.json();
     if (data.status === true) {
+        await getPosts();
+    }
+}
+
+function selectPost(post_id, title, body) {
+    id = post_id;
+    document.getElementById('title-edit').value = title;
+    document.getElementById('body-edit').value = body;
+}
+
+async function updatePost() {
+    const title = document.getElementById('title-edit').value,
+        body = document.getElementById('body-edit').value;
+    const data = {
+        title: title,
+        body: body
+    }
+    
+    const res = await fetch(`http://api.site31.ru/posts/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(data)
+    });
+
+    let resData = await res.json();
+    if (resData.status === true) {
         await getPosts();
     }
 }
